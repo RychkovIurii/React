898c59c4540f3b801b5c8e01d963a711fa58352f
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-09 17:22:50 +0300

    Hamburger and X for responsive version, switcher by click. Added transition to nav-links.

--- visit/src/component/Navbar.js
+++ visit/src/component/Navbar.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import { MenuItems } from './MenuItems';
+import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
 import "./NavbarStyles.css";
-import logo from '../images/logo_visit.png';
+import logo from "../images/logo_visit.png";
 
 export default class Navbar extends Component {
-  render() {
-	return (
-		<nav className='NavbarItems'>
-			<a className='navbar-logo' href="/"><img src={logo} width={100} height={100} alt="logo"/>Visit Central Asia</a>
-			<ul className='nav-menu'>
-				{MenuItems.map((item, index) => {
-					return(
-						<li key={index}>
-							<a className={item.cName} href={item.url}>
-							<i className={item.icon}></i>
-							{item.title}
-							</a>
-						</li>
-					)
-				})}
-			</ul>
-			<a href="./language"><i className="fa-solid fa-globe"></i>EN</a>
-		</nav>
-	)
-  }
+	state = { clicked: false };
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked });
+	}
+	render() {
+		return (
+			<nav className="NavbarItems">
+				<a className="navbar-logo" href="/"><img src={logo} width={100} height={100} alt="logo"/>Visit Central Asia</a>
+				<div className="menu-icons" onClick={this.handleClick}>
+					{/* <i className="fa-solid fa-bars"></i>
+					<i className="fas fa-times"></i> */}
+					<i className={this.state.clicked ? "fas fa-times" : "fa-solid fa-bars"}></i>
+				</div>
+				<ul className="nav-menu">
+					{MenuItems.map((item, index) => {
+						return(
+							<li key={index}>
+								<a className={item.cName} href={item.url}>
+								<i className={item.icon}></i>
+								{item.title}
+								</a>
+							</li>
+						)
+					})}
+				</ul>
+				<a href="./language"><i className="fa-solid fa-globe"></i>EN</a>
+			</nav>
+		)
+	}
 }
