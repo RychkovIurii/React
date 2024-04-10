cc42722a7a542427422cbed2abef34256f90aa57
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-10 19:46:43 +0300

    Prototyped ThingsToDo

--- visit/src/routes/ThingsToDo.js
+++ visit/src/routes/ThingsToDo.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
+import heroImage from '../assets/hero11.jpeg';
 
 function ThingsToDo(){
 	return(
 		<>
 			<Navbar/>
-			<h1>This is ThingsToDo</h1>
+			<Hero
+			cName = "heroToGo"
+			heroImage = {heroImage}
+			title = "Things to do"
+			text = "Enjoy Central Asia: Nature retreats to city festivals."
+			linkClass = "hide"
+			/>
 		</>
 	)
 }
