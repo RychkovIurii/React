58337605809ddffaa8e2c1c668928233ae2f2ebe
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-11 21:23:30 +0300

    Added scroll funct to Navbar

--- visit/src/components/Navbar.js
+++ visit/src/components/Navbar.js
@@ -5,20 +5,37 @@ import logo from "../images/logo_visit.png";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-	state = { clicked: false };
+	state = { clicked: false, isScrolled: false };
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
+
+	handleScroll = () => {
+		if (window.scrollY > 0) {
+			this.setState({ isScrolled: true });
+		} else {
+			this.setState({ isScrolled: false });
+		}
+	}
 	handleClick = () => {
 		this.setState({ clicked: !this.state.clicked });
 	}
 	render() {
+		const { clicked, isScrolled } = this.state;
 		return (
-			<nav className="NavbarItems">
+			<nav className={`NavbarItems ${isScrolled ? 'scrolled' : ''}`}>
 				<a className="navbar-logo" href="/"><img src={logo} width={100} height={100} alt="logo"/>Visit Central Asia</a>
 				<div className="menu-icons" onClick={this.handleClick}>
 					{/* <i className="fa-solid fa-bars"></i>
 					<i className="fas fa-times"></i> */}
-					<i className={this.state.clicked ? "fas fa-times" : "fa-solid fa-bars"}></i>
+					<i className={clicked ? "fas fa-times" : "fa-solid fa-bars"}></i>
 				</div>
-				<ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+				<ul className={clicked ? "nav-menu active" : "nav-menu"}>
 					{MenuItems.map((item, index) => {
 						return(
 							<li key={index}>
