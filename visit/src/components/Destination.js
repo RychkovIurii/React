9be681dbee6a8dd0d30366b368aa4592f67cfb7a
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-11 18:04:56 +0300

    Prototyped DestinationItems

--- visit/src/components/Destination.js
+++ visit/src/components/Destination.js
@@ -1,6 +1,7 @@
+import "./DestinationStyles.css";
+import DestinationItems from './DestinationItems';
 import destImage1 from '../assets/dest2.jpeg';
 import destImage2 from '../assets/dest1.jpeg';
-import "./DestinationStyles.css";
 
 const Destination = () => {
 	return(
@@ -8,18 +9,12 @@ const Destination = () => {
 			<h1>Popular Destinations</h1>
 			<p>Urban getaway or nature retreat? Find your perfect destination here.</p>
 
-			<div className="first-dest">
-				<div className="dest-text">
-					<h2>Almaty</h2>
-					<p>
-						Discover Almaty, the vibrant heart of Kazakhstan, where modern urban charm meets the wild beauty of nature. Nestled at the foot of the majestic Tien Shan mountains, Almaty is a city that captivates with its unique blend of history, culture, and breathtaking landscapes. From the tranquil greenery of Panfilov Park to the bustling streets of the city center, Almaty offers an array of experiences. Indulge in the local cuisine, explore ancient markets, or take a cable car ride to Kok-Tobe Hill for panoramic views that will leave you spellbound. Whether you're an adventure seeker or a culture enthusiast, Almaty promises unforgettable moments and discoveries at every turn. Plan your journey to Almaty today and embark on an adventure where the spirit of Kazakhstan comes to life.
-					</p>
-				</div>
-				<div className="image-dest">
-					<img alt="img" src={destImage1}/>
-					<img alt="img" src={destImage2}/>
-				</div>
-			</div>
+			<DestinationItems 
+			title = "Almaty"
+			text = "Discover Almaty, the vibrant heart of Kazakhstan, where modern urban charm meets the wild beauty of nature. Nestled at the foot of the majestic Tien Shan mountains, Almaty is a city that captivates with its unique blend of history, culture, and breathtaking landscapes. From the tranquil greenery of Panfilov Park to the bustling streets of the city center, Almaty offers an array of experiences. Indulge in the local cuisine, explore ancient markets, or take a cable car ride to Kok-Tobe Hill for panoramic views that will leave you spellbound. Whether you're an adventure seeker or a culture enthusiast, Almaty promises unforgettable moments and discoveries at every turn. Plan your journey to Almaty today and embark on an adventure where the spirit of Kazakhstan comes to life."
+			img1 = {destImage1}
+			img2 = {destImage2}
+			/>
 		</div>
 	)
 }
---
+++ visit/src/components/DestinationItems.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import "./DestinationStyles.css";
+
+export default class DestinationItems extends Component {
+  render() {
+	return (
+		<div className="first-dest">
+		<div className="dest-text">
+			<h2>{this.props.title}</h2>
+			<p>
+				{this.props.text}
+			</p>
+		</div>
+		<div className="image-dest">
+			<img alt="img" src={this.props.img1}/>
+			<img alt="img" src={this.props.img2}/>
+		</div>
+	</div>
+	)
+  }
+}
+
+
+
+
+
+/* export const DestinationItems = [
+	{
+		destTitle: "Almaty",
+		cNameImg: "image-dest",
+		cNameText: "dest-text",
+		cNameDest: "first-dest",
+		destText: "Discover Almaty, the vibrant heart of Kazakhstan, where modern urban charm meets the wild beauty of nature. Nestled at the foot of the majestic Tien Shan mountains, Almaty is a city that captivates with its unique blend of history, culture, and breathtaking landscapes. From the tranquil greenery of Panfilov Park to the bustling streets of the city center, Almaty offers an array of experiences. Indulge in the local cuisine, explore ancient markets, or take a cable car ride to Kok-Tobe Hill for panoramic views that will leave you spellbound. Whether you're an adventure seeker or a culture enthusiast, Almaty promises unforgettable moments and discoveries at every turn. Plan your journey to Almaty today and embark on an adventure where the spirit of Kazakhstan comes to life.",
+		url: "/"
+	},
+	{
+		destTitle: "Ala-kul",
+		cNameImg: "image-dest",
+		cNameText: "dest-text",
+		cNameDest: "first-dest-reverse",
+		destText: "Discover Almaty, the vibrant heart of Kazakhstan, where modern urban charm meets the wild beauty of nature. Nestled at the foot of the majestic Tien Shan mountains, Almaty is a city that captivates with its unique blend of history, culture, and breathtaking landscapes. From the tranquil greenery of Panfilov Park to the bustling streets of the city center, Almaty offers an array of experiences. Indulge in the local cuisine, explore ancient markets, or take a cable car ride to Kok-Tobe Hill for panoramic views that will leave you spellbound. Whether you're an adventure seeker or a culture enthusiast, Almaty promises unforgettable moments and discoveries at every turn. Plan your journey to Almaty today and embark on an adventure where the spirit of Kazakhstan comes to life.",
+		url: "/"
+	},
+	{
+		destTitle: "Silk Road",
+		url: "/",
+		cNameImg: "image-dest",
+		cNameText: "dest-text",
+		cNameDest: "first-dest"
+	},
+	{
+		destTitle: "Issyk Kul",
+		url: "/",
+		cNameImg: "image-dest",
+		cNameText: "dest-text",
+		cNameDest: "first-dest"
+	}
+] */
