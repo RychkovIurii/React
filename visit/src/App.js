1c329427c4ec7eeaf337429e59a75e6a31e949d0
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-13 19:47:02 +0300

    Added to app.js LangProvider

--- visit/src/App.js
+++ visit/src/App.js
@@ -6,12 +6,13 @@ import Search from './routes/Search';
 import PracticalTips from './routes/PracticalTips';
 import PlacesToGo from './routes/PlacesToGo';
 import PlacesToStay from './routes/PlacesToStay';
+import { LanguageProvider } from "./context/LanguageContext";
 
 function App() {
   return (
     <div className="App">
       
-
+      <LanguageProvider>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/places-to-stay' element={<PlacesToStay/>}/>
@@ -20,7 +21,7 @@ function App() {
         <Route path='/practical-tips' element={<PracticalTips/>}/>
         <Route path='/search' element={<Search/>}/>
       </Routes>
-
+      </LanguageProvider>
     </div>
   );
 }
