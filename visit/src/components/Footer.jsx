2897eb19b0d738a62750d8b619e4ae37e3dbf19c
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-13 23:33:03 +0300

    Connected LanguageContext to Destination and Footer

--- visit/src/components/Footer.jsx
+++ visit/src/components/Footer.jsx
@@ -1,50 +1,52 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { LanguageContext } from "../context/LanguageContext";
 import logo from "../images/logo_visit.png";
 import Weather from './Weather';
 
 export default function Footer() {
-  return (
-	<div className='footer'>
-		<div className='top'>
-			<a className="footer-logo" href="/"><img src={logo} width={80} height={80} alt="logo"/>Visit Central Asia</a>
-			<p>© 2024 Created by irychkov. All rights reserved.</p>
-			<div className='social-links'>
-				<a href='/'>
-					<i className='fa-brands fa-facebook-f'></i>
-				</a>
-				<a href='/'>
-					<i className='fa-brands fa-instagram'></i>
-				</a>
-				<a href='/'>
-					<i className='fa-brands fa-youtube'></i>
-				</a>
-				<a href='/'>
-					<i className='fa-brands fa-whatsapp'></i>
-				</a>
+	const { t } = useContext(LanguageContext);
+	return (
+		<div className='footer'>
+			<div className='top'>
+				<a className="footer-logo" href="/"><img src={logo} width={80} height={80} alt="logo"/>Visit Central Asia</a>
+				<p>{t("footer.footerP")}</p>
+				<div className='social-links'>
+					<a href='/'>
+						<i className='fa-brands fa-facebook-f'></i>
+					</a>
+					<a href='/'>
+						<i className='fa-brands fa-instagram'></i>
+					</a>
+					<a href='/'>
+						<i className='fa-brands fa-youtube'></i>
+					</a>
+					<a href='/'>
+						<i className='fa-brands fa-whatsapp'></i>
+					</a>
+				</div>
 			</div>
-		</div>
 
-		<div className='bottom'>
-			<div>
-				<h4>Contact</h4>
-				<a href='/'>Call us</a>
-				<a href='/'>Email us</a>
-				<a href='/'>Careers</a>
+			<div className='bottom'>
+				<div>
+					<h4>{t("footer.contact")}</h4>
+					<a href='/'>{t("footer.callUs")}</a>
+					<a href='/'>{t("footer.emailUs")}</a>
+					<a href='/'>{t("footer.careers")}</a>
+				</div>
+				<div>
+					<h4>{t("footer.about")}</h4>
+					<a href='/'>{t("footer.aboutUs")}</a>
+					<a href='/'>{t("footer.staff")}</a>
+					<a href='/'>{t("footer.partners")}</a>
+				</div>
+				<div>
+					<h4>{t("footer.others")}</h4>
+					<a href='/'>{t("footer.privacy")}</a>
+					<a href='/'>{t("footer.license")}</a>
+					<a href='/'>{t("footer.terms")}</a>
+				</div>
+				<Weather/>
 			</div>
-			<div>
-				<h4>About</h4>
-				<a href='/'>About us</a>
-				<a href='/'>Staff</a>
-				<a href='/'>Our Partners</a>
-			</div>
-			<div>
-				<h4>Others</h4>
-				<a href='/'>Privacy Policy</a>
-				<a href='/'>License</a>
-				<a href='/'>Terms of use</a>
-			</div>
-			<Weather/>
 		</div>
-	</div>
-  )
+	)
 }
