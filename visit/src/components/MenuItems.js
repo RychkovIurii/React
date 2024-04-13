c7c8e194dc8dee946c04915c044eade1a19481b9
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-14 00:24:48 +0300

    changed MenuItems for a connection to string.json

--- visit/src/components/MenuItems.js
+++ visit/src/components/MenuItems.js
@@ -1,34 +1,34 @@
 export const MenuItems = [
 	{
-		title: "Places to stay",
+		id: "placeToStay",
 		url: "/places-to-stay",
 		cName: "nav-links",
 		icon: "fa-solid fa-campground"
 	},
 	{
-		title: "Places to go",
+		id: "placeToGo",
 		url: "/places-to-go",
 		cName: "nav-links"
 	},
 	{
-		title: "Things to do",
+		id: "thingsToDo",
 		url: "/things-to-do",
 		cName: "nav-links"
 	},
 	{
-		title: "Practical tips",
+		id: "practicalTips",
 		url: "/practical-tips",
-		cName: "nav-links",
-	},
+		cName: "nav-links"
+	}
 	/* {
 		title: "EN",
 		url: "/",
 		cName: "nav-links",
 		icon: "fa-solid fa-globe"
 	}, */
-	{
+	/* {
 		url: "/search",
 		cName: "nav-links",
 		icon: "fa-solid fa-magnifying-glass"
-	}
+	} */
 ]
